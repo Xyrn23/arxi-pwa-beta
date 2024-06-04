08572036f20bfee996c1bd9bd3109b03a3ae08e5
Xyrn23 <jerseyaaronpaulreyes@gmail.com>
2024-06-04 17:20:55 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import PWABadge from "./PWABadge";
+import video from "./assets/second.mp4";
+
+const videoStyles = {
+  maxWidth: "50vw", // adjust the value to change the size
+  height: "auto",
+  borderRadius: "20px", // add border radius
+  boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.5)", // add drop shadow
+};
+
+function App() {
+  return (
+    <>
+      <main className="flex h-screen justify-center items-center">
+        <video autoPlay loop style={videoStyles} controls>
+          <source src={video} type="video/mp4" />
+        </video>
+      </main>
+      <PWABadge />
+    </>
+  );
+}
+
+export default App;
